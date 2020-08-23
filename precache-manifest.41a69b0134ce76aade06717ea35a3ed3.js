e8a3a6943a433ca97584194d30a19c9b1375e5e2
Fahad Shaikh <fahadnaeemsheikh@outlook.com>
2020-08-24 01:00:17 +0500

    Updates

---
+++ precache-manifest.41a69b0134ce76aade06717ea35a3ed3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d7eacfcd6318185d1b2adadabba4945",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cbc81104aaf996e7e60f",
+    "url": "/static/css/main.bf2e1e7a.chunk.css"
+  },
+  {
+    "revision": "863379499e7d2f0e2cf8",
+    "url": "/static/js/2.7f25d039.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7f25d039.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc81104aaf996e7e60f",
+    "url": "/static/js/main.ae6db399.chunk.js"
+  },
+  {
+    "revision": "dd8147bbe7734801bdde",
+    "url": "/static/js/runtime-main.e312f208.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "619740e42c388909f3e10192b1e26f05",
+    "url": "/static/media/MacBook.619740e4.png"
+  },
+  {
+    "revision": "56c354bcaa5cf3d46931cfa290a63bc3",
+    "url": "/static/media/MacBook1.56c354bc.png"
+  }
+]);
